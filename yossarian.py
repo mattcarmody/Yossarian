3475c54a07265c4184b061b1579bda6c56caf721
Matt Carmody <carmodym2@gmail.com>
2018-08-31 16:24:28 -0600

    Add command line option, list, to view available languages and actions.

--- yossarian.py
+++ yossarian.py
@@ -3,6 +3,7 @@
 import datetime
 import logging
 import sqlite3
+import sys
 
 logging.basicConfig(filename='yossarian_log.txt', level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
 
@@ -25,26 +26,37 @@ def main():
 	Esperanto = Language(languageNames[2])
 	Italian = Language(languageNames[3])
 	
-	now = str(datetime.datetime.now().timestamp())
-	
 	conn = sqlite3.connect("yossarian.db")
-	with conn:     
+	with conn:
 		cur = conn.cursor()
-		sql = 'SELECT PK, Language, Action, Quantity, Datetime FROM History'
-		cur.execute(sql)
-		raw_data = cur.fetchall()
-		for ii in range(len(raw_data)):
-			cur.execute("SELECT * FROM Actions WHERE Action=?", (raw_data[ii][2],))
-			weights = cur.fetchone()
-			quantity = raw_data[ii][3]
-			eval(raw_data[ii][1]).read += weights[2] * quantity /10
-			eval(raw_data[ii][1]).write += weights[3] * quantity /10
-			eval(raw_data[ii][1]).listen += weights[4] * quantity /10
-			eval(raw_data[ii][1]).speak += weights[5] * quantity /10
-		
-	for lang in languageNames:
-		for skill in languageSkills:
-			print("{} {}: {}".format(lang, skill, getattr(eval(lang), "{}".format(skill))))
+		if len(sys.argv) >= 2:
+			if sys.argv[1]=="list":
+				print("Available Languages:")
+				for name in languageNames:
+					print(name)
+				sql = "SELECT Action FROM Actions"
+				cur.execute(sql)
+				raw_data = cur.fetchall()
+				print("\nAvailable Actions:")
+				for ii in range(len(raw_data)):
+					print(raw_data[ii][0])
+		else:
+			now = str(datetime.datetime.now().timestamp())
+			sql = "SELECT PK, Language, Action, Quantity, Datetime FROM History"
+			cur.execute(sql)
+			raw_data = cur.fetchall()
+			for ii in range(len(raw_data)):
+				cur.execute("SELECT * FROM Actions WHERE Action=?", (raw_data[ii][2],))
+				weights = cur.fetchone()
+				quantity = raw_data[ii][3]
+				eval(raw_data[ii][1]).read += weights[2] * quantity
+				eval(raw_data[ii][1]).write += weights[3] * quantity
+				eval(raw_data[ii][1]).listen += weights[4] * quantity
+				eval(raw_data[ii][1]).speak += weights[5] * quantity
+			
+			for lang in languageNames:
+				for skill in languageSkills:
+					print("{} {}: {}".format(lang, skill, getattr(eval(lang), "{}".format(skill))))
 
 if __name__=="__main__":
 	main()
